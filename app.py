969ae9757f96c0d9f685cdb4b3c7c3b44c654313
SylvainBallerini <ballerini.sylvain@gmail.com>
2021-10-18 17:31:11 +0200

    graph death per million

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ import plotly.express as px
 
 data = pd.read_csv("owid-covid-data.csv")
 
-options = st.multiselect('What are your favorite colors',
-                        ['Green', 'Yellow', 'Red', 'Blue'],
-                        ['Yellow', 'Red'])
-
-st.write('You selected:', options)
-
 options_death = st.multiselect('Quel pays pour une comparaison ?', data['location'].unique(),['France'])
 
 #st.write('Le(s) pays sélectionnées : ', options_death)
@@ -21,5 +15,12 @@ fig = px.line(df, x='date', y='total_deaths', title="Nombres de morts par le Cov
 width=800)
 
 fig.update_layout(xaxis_title = 'Date', yaxis_title = 'Total de morts')
+
+st.plotly_chart(fig)
+
+fig = px.line(df, x='date', y='total_deaths_per_million', title="Nombres de morts par le Covid par millios d'habitants", color='location', 
+width=800)
+
+fig.update_layout(xaxis_title = 'Date', yaxis_title = "Total de morts par millions d'habitants")
 
 st.plotly_chart(fig)
